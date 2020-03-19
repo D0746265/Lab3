d91e15de10475caebd0d254f5877173a2b4f0e6e
user <user@FE07>
2020-03-19 14:34:24 +0800

    Test

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("tea set");System.out.print("test");
+		System.out.print("Hello everyone,\r\n");
+		System.out.print("I am studying at Feng Chia University.\r\n");
 
 	}
 
